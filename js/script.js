3a695585f39b4ea53333e4cfb770dbfff2ce3be3
Karen Okonkwo <karenokonkwo29@gmail.com>
2021-06-12 10:33:42 +0100

    Implement animations for page 1

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function makeProjectActive() {
+	const project = document.querySelector(".project");
+	console.log(project);
+	project.classList.toggle("project--active");
+	console.log(project);
+}
